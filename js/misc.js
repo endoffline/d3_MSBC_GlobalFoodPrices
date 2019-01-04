f393341401546f09648bc3322a08adf6b17005b6
endoffline <s1710629025@students.fh-hagenberg.at>
2019-01-04 23:42:02 +0100

    fixed country names in csv files

--- js/misc.js
+++ js/misc.js
@@ -7,17 +7,17 @@ function getYearItemsCount(flatData, year) {
 }
 
 function getCountryItemsCountDependingOnYear(flatData, year, country) {
-  return flatData.filter((d) => +d.year === +year && d.country === country).length;
+  return flatData.filter((d) => +d.year === +year && d.country === country && d.purchasingPowerParity).length;
 }
 
 function getCountryItemsCountDependingOnYearAndShortCommodities(flatData, year, shortCommodities, country) {
-  return flatData.filter((d) => +d.year === +year && shortCommodities.indexOf(d.shortCommodity) > -1 && d.country === country).length;
+  return flatData.filter((d) => +d.year === +year && shortCommodities.indexOf(d.shortCommodity) > -1 && d.country === country && d.purchasingPowerParity).length;
 }
 
 function getShortCommodityItemsCountDependingOnYear(flatData, year, shortCommodity) {
-  return flatData.filter((d) => +d.year === +year && d.shortCommodity === shortCommodity).length;
+  return flatData.filter((d) => +d.year === +year && d.shortCommodity === shortCommodity && d.purchasingPowerParity).length;
 }
 
 function getShortCommodityItemsCountDependingOnYearAndCountries(flatData, year, countries, shortCommodity) {
-  return flatData.filter((d) => +d.year === +year && countries.indexOf(d.country) > -1 && d.shortCommodity === shortCommodity).length;
+  return flatData.filter((d) => +d.year === +year && countries.indexOf(d.country) > -1 && d.shortCommodity === shortCommodity && d.purchasingPowerParity).length;
 }
