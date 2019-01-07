1fee6e0eddb866ae527a8dd9d815cd1ca0c6d5b6
endoffline <s1710629025@students.fh-hagenberg.at>
2019-01-07 17:38:04 +0100

    Cleaned up line graph code

--- js/linegraph.js
+++ js/linegraph.js
@@ -4,12 +4,11 @@
  * https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
  * https://blockbuilder.org/abrahamdu/65e36be64d281e3429b1fe238adabd25
  */
-// 2. Use the margin convention practice
+
 let margin = {top: 20, right: 200, bottom: 30, left: 30},
   width = document.getElementsByClassName('container')[0].clientWidth - margin.left - margin.right,
   height = width / 2 - margin.top - margin.bottom;
 
-// 1. Add the SVG to the page and employ #2
 let svgLine = d3.select('.lg-chart')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
@@ -22,11 +21,9 @@ let tooltip = d3.select('body')
   .style('opacity', 0);
 
 let xScale = d3.scaleLinear()
-  //.domain([0, n - 1]) // input
   .range([0, width]); // output
 
 let yScale = d3.scaleLinear()
-  //.domain([0, 1]) // input
   .range([height, 0]); // output
 
 let color = d3.scaleOrdinal().range(['#ffc107', '#f44336']);
@@ -38,7 +35,7 @@ let updateSelectedValuesHTML = function(country, commodity) {
 
 let initLineGraph = function (flatData, yearsSet, exchange, year, country, shortCommodity) {
   svgLine.selectAll('*').remove();
-  console.log(exchange + year + country + shortCommodity);
+
   // Filter the data according to the chosen values
   let selectedData = flatData.filter(
     (d) => d.year !== 2018 && d.country === country && d.shortCommodity === shortCommodity
@@ -77,43 +74,36 @@ let initLineGraph = function (flatData, yearsSet, exchange, year, country, short
         )
       }
     ];
-  console.log(transformedData);
-  console.log(selectedData);
-  console.log(yearsSet);
-// 7. d3's line generator
+
   xScale.domain(d3.extent(selectedData, (d) => d.year));
-  //let maxPrice = d3.max(selectedData, (d) => (exchange) ? d.meanDollarPpp : d.meanDollarEr);
   let maxPrice = d3.max(transformedData, (d) => d3.max(d.values, (dd) => dd.mean));
   yScale.domain([0, maxPrice]);
   
   color.domain(transformedData.map((d) => d.id));
   
+  // d3's line generator
   let line = d3.line()
-    .x(function (d, i) {
-      return xScale(d.year);
-    }) // set the x values for the line generator
-    .y(function (d) {
-      return yScale(d.mean);
-    }) // set the y values for the line generator
+    .x((d) => xScale(d.year)) // set the x values for the line generator
+    .y((d) => yScale(d.mean)) // set the y values for the line generator
     .curve(d3.curveMonotoneX) // apply smoothing to the line
   
-// 3. Call the x axis in a group tag
-  svgLine.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+  // Call the x axis in a group tag
+  svgLine.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-// 4. Call the y axis in a group tag
-  svgLine.append("g")
-    .attr("class", "y axis")
+  // Call the y axis in a group tag
+  svgLine.append('g')
+    .attr('class', 'y axis')
     .call(d3.axisLeft(yScale))
-    .append("text")
-    .attr("x", 5)
-    .attr("y", yScale(yScale.ticks().pop()) - 10)
-    .attr("dy", "0.32em")
-    .attr("text-anchor", "start")
-    .attr("font-size", 14)
-    .text(" Price in USD");
+    .append('text')
+    .attr('x', 5)
+    .attr('y', yScale(yScale.ticks().pop()) - 10)
+    .attr('dy', '0.32em')
+    .attr('text-anchor', 'start')
+    .attr('font-size', 14)
+    .text(' Price in USD');
 
 
   let lines = svgLine
@@ -125,22 +115,22 @@ let initLineGraph = function (flatData, yearsSet, exchange, year, country, short
     .enter()
     .append('g')
     .attr('class', 'line-group')
-    .append("path")
-    .attr("class", "line") // Assign a class for styling
-    .attr("d", (d) => line(d.values)) // 11. Calls the line generator
-    .style("stroke", (d, i) => color(i));
+    .append('path')
+    .attr('class', 'line') // Assign a class for styling
+    .attr('d', (d) => line(d.values)) // 11. Calls the line generator
+    .style('stroke', (d, i) => color(i));
     
   lines.selectAll('.line-group')
-    .append("text")
+    .append('text')
     .datum((d) => { return {id: d.id, value: d.values[0]} })
-    .attr("transform", function(d) { return "translate(" + xScale(d.value.year) + "," + yScale(d.value.mean) + ")"; })
-    .attr("x", 10)
-    .attr("dy", "0.35em")
-    .style("font", "14px")
-    .text(function(d) { return d.id; });
+    .attr('transform', (d) => { return 'translate(' + xScale(d.value.year) + ',' + yScale(d.value.mean) + ')'; })
+    .attr('x', 10)
+    .attr('dy', '0.35em')
+    .style('font', '14px')
+    .text((d) => d.id);
   
-// 12. Appends a circle for each datapoint
-  svgLine.selectAll(".circle-group")
+  // Appends a circle for each datapoint
+  svgLine.selectAll('.circle-group')
     .data(transformedData)
     .enter()
     .append('g')
@@ -149,11 +139,11 @@ let initLineGraph = function (flatData, yearsSet, exchange, year, country, short
     .data((d) => d.values)
     .enter()
     .append('g') // Uses the enter().append() method
-    .attr("class", "circle") // Assign a class for styling
+    .attr('class', 'circle') // Assign a class for styling
     .append('circle')
-    .attr("cx", (d) => xScale(d.year))
-    .attr("cy", (d) => yScale(d.mean))
-    .attr("r", 4)
+    .attr('cx', (d) => xScale(d.year))
+    .attr('cy', (d) => yScale(d.mean))
+    .attr('r', 4)
     .on('mouseover', function (d) {
       tooltip.transition()
         .duration(200)
@@ -170,7 +160,7 @@ let initLineGraph = function (flatData, yearsSet, exchange, year, country, short
       d3.select(this)
         .transition()
         .duration(200)
-        .attr("r", 6);
+        .attr('r', 6);
     })
     .on('mouseout', function () {
       tooltip.transition()
@@ -180,7 +170,7 @@ let initLineGraph = function (flatData, yearsSet, exchange, year, country, short
       d3.select(this)
         .transition()
         .duration(200)
-        .attr("r", 4);
+        .attr('r', 4);
     })
   ;
   
